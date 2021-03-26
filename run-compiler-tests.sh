1ed45da38af81470367f63abdb881d3bbf4f65aa
John Haiducek <jhaiduce@gmail.com>
2021-03-26 11:46:09 -0400

    Remove --progress=plain option

--- run-compiler-tests.sh
+++ run-compiler-tests.sh
@@ -6,4 +6,4 @@ LOCAL_REPO_DIR=/Users/jhaiduce/Development/repositories
 
 docker build -t lightda-examples-src -f Dockerfile.src .
 docker build -t lightda-examples-dependency-repos -f Dockerfile.repos $LOCAL_REPO_DIR
-docker build --progress=plain --build-arg compiler_image=gcc:10.2.0 compiler-tests/gcc/10.2.0
+docker build --build-arg compiler_image=gcc:10.2.0 compiler-tests/gcc/10.2.0
