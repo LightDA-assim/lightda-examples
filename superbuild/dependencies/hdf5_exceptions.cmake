136d80b78d99d6c0c8d1a4b1aff8939138970756
John Haiducek <jhaiduce@gmail.com>
2021-03-26 12:32:16 -0400

    Add fortran_exceptions to dependency list for hdf5_exceptions

--- superbuild/dependencies/hdf5_exceptions.cmake
+++ superbuild/dependencies/hdf5_exceptions.cmake
@@ -21,4 +21,4 @@ ExternalProject_Add(
     -Dfortran_exceptions_DIR:PATH=${fortran_exceptions_DIR}
     -Dhdf5_DIR:PATH=${HDF5_DIR}
     -DCMAKE_INSTALL_PREFIX:PATH=${hdf5_exceptions_INSTALL_DIR}
-  DEPENDS HDF5)
+  DEPENDS HDF5 fortran_exceptions)
