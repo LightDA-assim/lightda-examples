305303038efc648ffee4461bd817f0325c7b810a
John Haiducek <jhaiduce@gmail.com>
2021-04-06 15:50:40 -0400

    Don't pass HDF5_ROOT as a CMake option since FindHDF5 looks instead for an environment variable

--- superbuild/dependencies/hdf5_exceptions.cmake
+++ superbuild/dependencies/hdf5_exceptions.cmake
@@ -21,6 +21,5 @@ ExternalProject_Add(
     -Dfortran_exceptions_DIR:PATH=${fortran_exceptions_DIR}
     -Dhdf5_DIR:PATH=${HDF5_DIR}
     -DHDF5_DIR:PATH=${HDF5_DIR}
-    -DHDF5_ROOT:PATH=${HDF5_ROOT}
     -DCMAKE_INSTALL_PREFIX:PATH=${hdf5_exceptions_INSTALL_DIR}
   DEPENDS HDF5 fortran_exceptions)
