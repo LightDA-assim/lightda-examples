1686db2b3079a95c037ad1c060544e50204fdfbe
John Haiducek <jhaiduce@gmail.com>
2021-04-06 14:40:48 -0400

    Set UPDATE_COMMAND argument conditionally dependong on git version

--- superbuild/dependencies/hdf5.cmake
+++ superbuild/dependencies/hdf5.cmake
@@ -18,12 +18,18 @@ if(NOT HDF5_FOUND AND NOT hdf5_FOUND
     AND NOT TARGET hdf5::hdf5_fortran
     AND NOT TARGET hdf5_fortran-static
     AND NOT TARGET hdf5_fortran-shared)
+  if(NOT ${CMAKE_VERSION} VERSION_LESS 3.2)
+    # Setting UPDATE_COMMAND to "" saves time when re-building, but is
+    # not supported on older CMake versions
+    set(HDF5_PROJECT_UPDATE_DISCONNECTED_ARG "UPDATE_DISCONNECTED OFF")
+  endif()
+
   ExternalProject_Add(
     HDF5
     GIT_REPOSITORY ${HDF5_GIT_URL}
     GIT_TAG hdf5-1_12_0
     UPDATE_COMMAND ""
-    UPDATE_DISCONNECTED OFF
+    ${HDF5_PROJECT_UPDATE_DISCONNECTED_ARG}
     CMAKE_CACHE_ARGS
       -DHDF5_BUILD_FORTRAN:BOOL=ON
       -DCMAKE_INSTALL_PREFIX:PATH=${HDF5_INSTALL_DIR})
