c1772f3542f5a3d7dd97fde121d25e8d39cd7e30
John Haiducek <jhaiduce@gmail.com>
2021-04-06 15:50:08 -0400

    Build with gcc 4.9 before gcc 10.2

--- run-compiler-tests.sh
+++ run-compiler-tests.sh
@@ -10,8 +10,8 @@ docker build -t lightda-examples-dependency-repos -f Dockerfile.repos $LOCAL_REP
 docker build -t lightda-examples-gcc:latest --build-arg compiler_image=gcc:latest compiler-tests
 docker build -t lightda-examples-oneapi:2021.2-devel-centos8 --build-arg compiler_image=intel/oneapi-hpckit:2021.2-devel-centos8 --build-arg CC=icc --build-arg CXX=icpc --build-arg FC=ifort --build-arg FFLAGS="$IFORT_FFLAGS" compiler-tests
 docker build -t lightda-examples-oneapi:2021.2-devel-ubuntu18.04 --build-arg compiler_image=intel/oneapi-hpckit:2021.2-devel-ubuntu18.04 --build-arg CC=icc --build-arg CXX=icpc --build-arg FC=ifort --build-arg FFLAGS="$IFORT_FFLAGS" compiler-tests
-docker build -t lightda-examples-gcc:10.2.0 --build-arg compiler_image=gcc:10.2.0 compiler-tests
 docker build -t lightda-examples-gcc:4.9.4-hdf5-1_10-7 --build-arg compiler_image=gcc:4.9.4 --build-arg hdf5_tag=hdf5-1_10_7 compiler-tests
+docker build -t lightda-examples-gcc:10.2.0 --build-arg compiler_image=gcc:10.2.0 compiler-tests
 docker build -t lightda-examples-gcc:5.5.0-hdf5-1_10-7 --build-arg compiler_image=gcc:5.5.0 --build-arg hdf5_tag=hdf5-1_10_7 compiler-tests
 docker build -t lightda-examples-gcc:6.5.0-hdf5-1_10-7 --build-arg compiler_image=gcc:6.5.0 --build-arg hdf5_tag=hdf5-1_10_7 compiler-tests
 docker build -t lightda-examples-gcc:7.5.0-hdf5-1_10-7 --build-arg compiler_image=gcc:7.5.0 --build-arg hdf5_tag=hdf5-1_10_7 compiler-tests
