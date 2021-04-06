63b8e29ab8878310deed9e27b7077c74caa1bdf2
John Haiducek <jhaiduce@gmail.com>
2021-04-06 14:42:37 -0400

    Change order of if statement (doing the simple case first to aid in readability)

--- superbuild/dependencies/hdf5.cmake
+++ superbuild/dependencies/hdf5.cmake
@@ -14,10 +14,19 @@ enable_language(Fortran)
 find_package(HDF5 COMPONENTS Fortran QUIET)
 find_package(hdf5 COMPONENTS Fortran QUIET)
 
-if(NOT HDF5_FOUND AND NOT hdf5_FOUND
-    AND NOT TARGET hdf5::hdf5_fortran
-    AND NOT TARGET hdf5_fortran-static
-    AND NOT TARGET hdf5_fortran-shared)
+if(HDF5_FOUND OR hdf5_FOUND
+    OR TARGET hdf5::hdf5_fortran
+    OR TARGET hdf5_fortran-static
+    OR TARGET hdf5_fortran-shared)
+
+  # HDF5 is already installed, create a custom target to satisfy components that
+  # need it
+  add_custom_target(HDF5)
+
+else()
+
+  # HDF5 is not found, try to download and build it
+
   if(NOT ${CMAKE_VERSION} VERSION_LESS 3.2)
     # Setting UPDATE_COMMAND to "" saves time when re-building, but is
     # not supported on older CMake versions
@@ -33,8 +42,7 @@ if(NOT HDF5_FOUND AND NOT hdf5_FOUND
     CMAKE_CACHE_ARGS
       -DHDF5_BUILD_FORTRAN:BOOL=ON
       -DCMAKE_INSTALL_PREFIX:PATH=${HDF5_INSTALL_DIR})
-  set(HDF5_DIR ${HDF5_INSTALL_DIR}/share/cmake/hdf5)
-else()
-  add_custom_target(HDF5)
-endif()
 
+  set(HDF5_DIR ${HDF5_INSTALL_DIR}/share/cmake/hdf5)
+
+endif()
