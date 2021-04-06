cf8630a311161a121cef7042f3a91c2feb8a49a8
John Haiducek <jhaiduce@gmail.com>
2021-04-06 15:48:38 -0400

    Build HDF5 using autotools if CMake version is less than 3.12

--- superbuild/dependencies/hdf5.cmake
+++ superbuild/dependencies/hdf5.cmake
@@ -5,6 +5,11 @@ set(
   "https://github.com/HDFGroup/hdf5.git" CACHE STRING
   "URL of HDF5 git repository")
 
+set(
+  HDF5_GIT_TAG
+  "hdf5-1_12_0" CACHE STRING
+  "URL of HDF5 git repository")
+
 set(HDF5_INSTALL_DIR
   "${CMAKE_CURRENT_BINARY_DIR}/HDF5" CACHE PATH
   "HDF5 installation directory")
@@ -27,22 +32,32 @@ else()
 
   # HDF5 is not found, try to download and build it
 
-  if(NOT ${CMAKE_VERSION} VERSION_LESS 3.2)
-    # Setting UPDATE_COMMAND to "" saves time when re-building, but is
-    # not supported on older CMake versions
-    set(HDF5_PROJECT_UPDATE_DISCONNECTED_ARG "UPDATE_DISCONNECTED OFF")
+  if(${CMAKE_VERSION} VERSION_LESS 3.12)
+
+    # Build HDF5 with configure/make
+    ExternalProject_Add(
+      HDF5
+      GIT_REPOSITORY ${HDF5_GIT_URL}
+      GIT_TAG ${HDF5_GIT_TAG}
+      CONFIGURE_COMMAND ${CMAKE_CURRENT_BINARY_DIR}/HDF5-prefix/src/HDF5/configure --enable-fortran --prefix=${HDF5_INSTALL_DIR}
+      BUILD_COMMAND ${MAKE})
+
+    set(ENV{HDF5_ROOT} ${HDF5_INSTALL_DIR} PARENT_SCOPE)
+
+  else()
+
+    # Build HDF5 with CMake
+    ExternalProject_Add(
+      HDF5
+      GIT_REPOSITORY ${HDF5_GIT_URL}
+      GIT_TAG ${HDF5_GIT_TAG}
+      UPDATE_COMMAND ""
+      CMAKE_CACHE_ARGS
+        -DHDF5_BUILD_FORTRAN:BOOL=ON
+        -DCMAKE_INSTALL_PREFIX:PATH=${HDF5_INSTALL_DIR})
+
+    set(HDF5_DIR ${HDF5_INSTALL_DIR}/share/cmake/hdf5)
+
   endif()
-
-  ExternalProject_Add(
-    HDF5
-    GIT_REPOSITORY ${HDF5_GIT_URL}
-    GIT_TAG hdf5-1_12_0
-    UPDATE_COMMAND ""
-    ${HDF5_PROJECT_UPDATE_DISCONNECTED_ARG}
-    CMAKE_CACHE_ARGS
-      -DHDF5_BUILD_FORTRAN:BOOL=ON
-      -DCMAKE_INSTALL_PREFIX:PATH=${HDF5_INSTALL_DIR})
-
-  set(HDF5_DIR ${HDF5_INSTALL_DIR}/share/cmake/hdf5)
 
 endif()
