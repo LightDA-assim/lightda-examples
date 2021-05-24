399521a9036ebc9731709febe071fee556255316
John Haiducek <jhaiduce@gmail.com>
2021-05-24 15:08:01 -0400

    Build each LightDA component in its own stage, to take better advantage of the Docker build cache

--- run-compiler-tests.sh
+++ run-compiler-tests.sh
@@ -6,7 +6,11 @@ LOCAL_REPO_DIR=/Users/jhaiduce/Development/repositories
 IFORT_FFLAGS="-g -traceback -check all -standard-semantics -assume nostd_mod_proc_name -stand f08"
 
 docker build -t lightda-examples-src -f Dockerfile.src .
-docker build -t lightda-examples-dependency-repos -f Dockerfile.repos $LOCAL_REPO_DIR
+docker build -t system_mpi-repo --build-arg repo=system_mpi.git -f Dockerfile.repos $LOCAL_REPO_DIR
+docker build -t fortran_exceptions-repo --build-arg repo=fortran_exceptions.git -f Dockerfile.repos $LOCAL_REPO_DIR
+docker build -t hdf5_exceptions-repo --build-arg repo=hdf5_exceptions.git -f Dockerfile.repos $LOCAL_REPO_DIR
+docker build -t lightda-repo --build-arg repo=lightda.git -f Dockerfile.repos $LOCAL_REPO_DIR
+docker build -t lightda_lenkf_rsm-repo --build-arg repo=lightda_lenkf_rsm.git -f Dockerfile.repos $LOCAL_REPO_DIR
 docker build -t lightda-examples-gcc:latest --build-arg compiler_image=gcc:latest compiler-tests
 docker build -t lightda-examples-oneapi:2021.2-devel-centos8 --build-arg compiler_image=intel/oneapi-hpckit:2021.2-devel-centos8 --build-arg CC=icc --build-arg CXX=icpc --build-arg FC=ifort --build-arg FFLAGS="$IFORT_FFLAGS" compiler-tests
 docker build -t lightda-examples-oneapi:2021.2-devel-ubuntu18.04 --build-arg compiler_image=intel/oneapi-hpckit:2021.2-devel-ubuntu18.04 --build-arg CC=icc --build-arg CXX=icpc --build-arg FC=ifort --build-arg FFLAGS="$IFORT_FFLAGS" compiler-tests
